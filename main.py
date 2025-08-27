472c081da06152f7b415f132d89f201034ea682c
Tomer <tomer@jeepsea.co.il>
2025-08-27 20:18:41 +0300

    Complete NN to train and test on MNIST

--- main.py
+++ main.py
@@ -1,17 +1,45 @@
-from tensorflow.keras.datasets import mnist
-import matplotlib.pyplot as plt
-import numpy as np
+from os.path import split
+import tensorflow as tf
+import tensorflow_datasets as tfds
+(ds_train, ds_test), ds_info = tfds.load(
+    'mnist',
+    split=['train', 'test'],
+    shuffle_files=True,
+    as_supervised=True,
+    with_info=True,
+)
 
-#load the MNIST dataset
-(X_train, y_train), (_, _) = mnist.load_data()
 
-#print the first 4 pictures in a row
-plt.figure(figsize=(10, 5))
-for i in range(4):
-    plt.subplot(1, 4, i+1)
-    plt.imshow(X_train[i], cmap='gray')
-    plt.title(f"Label: {y_train[i]}")
-    plt.axis('off')
-plt.tight_layout()
-plt.show()
+def normalize_img(image, label):
+    return tf.cast(image, tf.float32) / 255., label
 
+
+ds_train = ds_train.map(
+    normalize_img, num_parallel_calls=tf.data.AUTOTUNE)
+ds_train = ds_train.cache()
+ds_train = ds_train.shuffle(ds_info.splits['train'].num_examples)
+ds_train = ds_train.batch(128)
+ds_train = ds_train.prefetch(tf.data.AUTOTUNE)
+
+ds_test = ds_test.map(
+    normalize_img, num_parallel_calls=tf.data.AUTOTUNE)
+ds_test = ds_test.batch(128)
+ds_test = ds_test.cache()
+ds_test = ds_test.prefetch(tf.data.AUTOTUNE)
+
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Flatten(input_shape=(28,28)),
+    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dense(10)
+])
+model.compile(
+    optimizer=tf.keras.optimizers.Adam(0.001),
+    loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+    metrics=[tf.keras.metrics.SparseCategoricalCrossentropy()],
+)
+
+model.fit(
+    ds_train,
+    epochs=6,
+    validation_data=ds_test,
+)
