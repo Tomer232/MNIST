7a912dcd34aa76bf1dbf5ba29372d966bc11586b
Tomer <tomer@jeepsea.co.il>
2025-08-27 19:24:11 +0300

    loading MNIST dataset

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("hello world")
+from tensorflow.keras.datasets import mnist
+import matplotlib.pyplot as plt
+import numpy as np
+
+#load the MNIST dataset
+(X_train, y_train), (_, _) = mnist.load_data()
+
+#print the first 4 pictures in a row
+plt.figure(figsize=(10, 5))
+for i in range(4):
+    plt.subplot(1, 4, i+1)
+    plt.imshow(X_train[i], cmap='gray')
+    plt.title(f"Label: {y_train[i]}")
+    plt.axis('off')
+plt.tight_layout()
+plt.show()
+
