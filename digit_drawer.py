1084c76c6ddc51adbca4c07989a048f260bcbfbe
Tomer <tomer@jeepsea.co.il>
2025-08-29 16:52:56 +0300

    constants for drwing window.

---
+++ digit_drawer.py
@@ -0,0 +1,18 @@
+import pygame
+import numpy as np
+import tensorflow as tf
+from PIL import Image
+from pygame.examples.grid import WINDOW_HEIGHT
+
+CANVAS_SIZE = 280
+WINDOW_SIZE = 500
+WINDOW_HEIGHT = 350
+CANVAS_X = 10
+CANVAS_Y = 35
+PREDICTION_X = 310
+PREDICTION_Y = 50
+
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GRAY = (128, 128, 128)
+
