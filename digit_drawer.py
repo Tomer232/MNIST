fbc434cf33f956d25e7c28dd4938a652fda6e6d4
Tomer <tomer@jeepsea.co.il>
2025-08-29 16:58:27 +0300

    init the game

--- digit_drawer.py
+++ digit_drawer.py
@@ -2,10 +2,9 @@ import pygame
 import numpy as np
 import tensorflow as tf
 from PIL import Image
-from pygame.examples.grid import WINDOW_HEIGHT
 
 CANVAS_SIZE = 280
-WINDOW_SIZE = 500
+WINDOW_WIDTH = 500
 WINDOW_HEIGHT = 350
 CANVAS_X = 10
 CANVAS_Y = 35
@@ -15,4 +14,19 @@ PREDICTION_Y = 50
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 GRAY = (128, 128, 128)
+
+pygame.init()
+screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+pygame.display.set_caption("MNIST Digit Recognizer")
+
+print("Loading model...")
+model = tf.keras.models.load_model('mnist_model.h5')
+print("Model loaded successfully")
+
+canvas = pygame.Surface((CANVAS_SIZE, CANVAS_SIZE))
+canvas.fill(BLACK)
+
+drawing = False
+clock = pygame.time.Clock()
+
 
